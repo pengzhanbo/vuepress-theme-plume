20340f735f30399e10b1089f8e207010e8af35b5
pengzhanbo <volodymyr@foxmail.com>
2023-02-10 04:37:08 +0800

    feat(theme): add global component `NavbarSearch`

--- packages/theme/src/client/config.ts
+++ packages/theme/src/client/config.ts
@@ -1,4 +1,5 @@
 import { defineClientConfig } from '@vuepress/client'
+import { h } from 'vue'
 import { setupDarkMode } from './composables/index.js'
 import Layout from './layouts/Layout.vue'
 import NotFound from './layouts/NotFound.vue'
@@ -6,6 +7,17 @@ import NotFound from './layouts/NotFound.vue'
 import './styles/index.scss'
 
 export default defineClientConfig({
+  enhance({ app }) {
+    // eslint-disable-next-line vue/match-component-file-name
+    app.component('NavbarSearch', () => {
+      const SearchComponent =
+        app.component('Docsearch') || app.component('SearchBox')
+      if (SearchComponent) {
+        return h(SearchComponent)
+      }
+      return null
+    })
+  },
   setup() {
     setupDarkMode()
   },

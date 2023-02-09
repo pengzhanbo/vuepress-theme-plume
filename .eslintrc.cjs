12bcd2c0bcd3f38acdf2976447cca7620faa1831
pengzhanbo <volodymyr@foxmail.com>
2023-02-10 04:31:00 +0800

    style: off `explicit-function-return-type`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
         'vue/component-tags-order': [
           'error',
           {

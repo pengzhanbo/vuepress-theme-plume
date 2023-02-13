2fe31042531203100ea6c83063fa51551fa929be
pengzhanbo <volodymyr@foxmail.com>
2023-02-14 05:01:43 +0800

    feat(theme): add theme default options

--- packages/theme/src/node/defaultOptions.ts
+++ packages/theme/src/node/defaultOptions.ts
@@ -4,6 +4,15 @@ import type { PlumeThemeLocaleOptions } from '../shared/index.js'
 export const defaultLocaleOption: Partial<PlumeThemeLocaleOptions> = {
   article: '/article',
   notes: { link: '/note', dir: 'notes', notes: [] },
+  blog: {
+    include: ['**/*.md'],
+    exclude: ['.vuepress/', 'node_modules/', '{README,index}.md'],
+    link: '/blog/',
+  },
+  footer: {
+    message:
+      'Power by <a target="_blank" href="https://v2.vuepress.vuejs.org/">Vuepress</a> & <a target="_blank" href="https://github.com/pengzhanbo/vuepress-theme-plume">vuepress-theme-plume</a>',
+  },
 }
 
 export const mergeLocaleOptions = (options: PlumeThemeLocaleOptions) => {

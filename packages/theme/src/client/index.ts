3fdff1e96b5a698699fde17cd5fe10a01492632c
pengzhanbo <volodymyr@foxmail.com>
2023-02-15 00:50:42 +0800

    build(theme): add exports

--- packages/theme/src/client/index.ts
+++ packages/theme/src/client/index.ts
@@ -1,1 +1,6 @@
 export * from '../shared/index.js'
+
+export { default as plumeClientConfig } from './config.js'
+
+export { default as Layout } from './layouts/Layout.vue'
+export { default as NotFound } from './layouts/NotFound.vue'

95542b82de052211b2c27bcd35cca260e7e2964a
pengzhanbo <volodymyr@foxmail.com>
2023-07-29 03:44:14 +0800

    style: fix lint

--- packages/plugin-blog-data/src/client/index.ts
+++ packages/plugin-blog-data/src/client/index.ts
@@ -1,4 +1,4 @@
 import type { BlogPostData, BlogPostDataItem } from '../shared/index.js'
 export * from './composables/index.js'
 
-export { BlogPostData, BlogPostDataItem }
+export type { BlogPostData, BlogPostDataItem }

b9a8492e081b3555935c247c8c1180cf35e6347b
pengzhanbo <volodymyr@foxmail.com>
2023-07-29 03:33:51 +0800

    build: update deps to latest

--- packages/plugin-auto-frontmatter/src/node/index.ts
+++ packages/plugin-auto-frontmatter/src/node/index.ts
@@ -7,6 +7,6 @@ import { autoFrontmatterPlugin } from './plugin.js'
 
 export * from './plugin.js'
 
-export { AutoFrontmatterOptions, FrontmatterArray, FrontmatterObject }
+export type { AutoFrontmatterOptions, FrontmatterArray, FrontmatterObject }
 
 export default autoFrontmatterPlugin

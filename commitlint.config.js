1eb541efae2e8117bd177b673de9d113209870fe
pengzhanbo <volodymyr@foxmail.com>
2024-03-04 03:44:17 +0800

    perf: update `vue` to latest

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,12 @@
-const fs = require('node:fs')
-const path = require('node:path')
+import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const packages = fs.readdirSync(path.resolve(__dirname, 'plugins'))
 
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-enum': [2, 'always', ['docs', 'theme', ...packages]],

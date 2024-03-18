7d7dede526eaf994fdd22251144a924c05c5ec04
pengzhanbo <volodymyr@foxmail.com>
2024-03-19 00:35:39 +0800

    feat(theme): 博主信息新增 地理位置和所属组织

---
+++ theme/src/client/components/TransitionFadeSlideY.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { Transition } from 'vue'
+import { useScrollPromise } from '../composables/index.js'
+
+const { resolve: onBeforeEnter, pending: onBeforeLeave } = useScrollPromise()
+</script>
+
+<template>
+  <Transition
+    name="fade-slide-y"
+    mode="out-in"
+    @before-enter="onBeforeEnter"
+    @before-leave="onBeforeLeave"
+  >
+    <slot />
+  </Transition>
+</template>
---
+++ theme/src/client/components/icons/IconLocation.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+      <circle cx="12" cy="10" r="3" />
+      <path d="M12 2a8 8 0 0 0-8 8c0 1.892.402 3.13 1.5 4.5L12 22l6.5-7.5c1.098-1.37 1.5-2.608 1.5-4.5a8 8 0 0 0-8-8" />
+    </g>
+  </svg>
+</template>

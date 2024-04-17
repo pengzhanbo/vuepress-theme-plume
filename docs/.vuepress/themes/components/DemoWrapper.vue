f1570e49a80782af924f321afe2fdd53ab25044b
pengzhanbo <volodymyr@foxmail.com>
2024-04-17 12:13:43 +0800

    docs: 添加首页色板可视化配置工具

---
+++ docs/.vuepress/themes/components/DemoWrapper.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="demo-wrapper no-padding has-title">
+    <div class="demo-head">
+      <div class="demo-ctrl">
+        <i /><i /><i />
+      </div>
+    </div>
+    <div class="demo-container">
+      <slot />
+    </div>
+  </div>
+</template>

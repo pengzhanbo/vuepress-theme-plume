5a54fc2565178fec03a684c000f4027f256b9ec7
pengzhanbo <volodymyr@foxmail.com>
2024-06-18 14:37:38 +0800

    perf(theme): 优化页面滚动行为

---
+++ theme/src/client/composables/scroll-behavior.ts
@@ -0,0 +1,25 @@
+import type { Router } from 'vuepress/client'
+import { nextTick } from 'vue'
+import { inBrowser } from '../utils/index.js'
+import { useScrollPromise } from './scroll-promise.js'
+
+export function enhanceScrollBehavior(router: Router) {
+  // handle scrollBehavior with transition
+  const scrollBehavior = router.options.scrollBehavior!
+  router.options.scrollBehavior = async (...args) => {
+    await useScrollPromise().wait()
+    return scrollBehavior(...args)
+  }
+
+  router.beforeEach(() => {
+    if (inBrowser) {
+      document.documentElement.classList.remove('smooth')
+    }
+  })
+
+  router.afterEach(() => nextTick(() => {
+    if (inBrowser) {
+      document.documentElement.classList.add('smooth')
+    }
+  }))
+}
--- theme/src/client/config.ts
+++ theme/src/client/config.ts
@@ -4,7 +4,7 @@ import { defineClientConfig } from 'vuepress/client'
 import type { ClientConfig } from 'vuepress/client'
 import { h } from 'vue'
 import VPBadge from './components/global/VPBadge.vue'
-import { setupDarkMode, setupWatermark, useScrollPromise } from './composables/index.js'
+import { enhanceScrollBehavior, setupDarkMode, setupWatermark } from './composables/index.js'
 import Layout from './layouts/Layout.vue'
 import NotFound from './layouts/NotFound.vue'
 import VPHomeBox from './components/Home/VPHomeBox.vue'
@@ -12,6 +12,8 @@ import VPHomeBox from './components/Home/VPHomeBox.vue'
 export default defineClientConfig({
   enhance({ app, router }) {
     setupDarkMode(app)
+    enhanceScrollBehavior(router)
+
     // global component
     app.component('Badge', VPBadge)
     app.component('VPBadge', VPBadge) // alias
@@ -44,13 +46,6 @@ export default defineClientConfig({
     /** @deprecated */
     app.component('HomeBox', VPHomeBox)
     app.component('VPHomeBox', VPHomeBox)
-
-    // handle scrollBehavior with transition
-    const scrollBehavior = router.options.scrollBehavior!
-    router.options.scrollBehavior = async (...args) => {
-      await useScrollPromise().wait()
-      return scrollBehavior(...args)
-    }
   },
   setup() {
     setupWatermark()

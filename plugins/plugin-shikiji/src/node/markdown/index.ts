94360723d41fc1633aa55a06460fa0749d5009fb
pengzhanbo <volodymyr@foxmail.com>
2024-09-15 02:09:34 +0800

    perf(plugin-md-power): optimize `collapsed-lines`

--- plugins/plugin-shikiji/src/node/markdown/index.ts
+++ plugins/plugin-shikiji/src/node/markdown/index.ts
@@ -1,3 +1,4 @@
+export * from './collapsedLinesPlugin.js'
 export * from './highlightLinesPlugin.js'
 export * from './lineNumberPlugin.js'
 export * from './preWrapperPlugin.js'
--- plugins/plugin-shikiji/src/node/utils/collapsedLines.ts
+++ plugins/plugin-shikiji/src/node/utils/collapsedLines.ts
@@ -1,18 +1,24 @@
-export const COLLAPSED_LINES_REGEXP = /:collapsed-lines(?:=(\d+))?\b/
-export const NO_COLLAPSED_LINES_REGEXP = /:no-collapsed-lines\b/
+const COLLAPSED_LINES_REGEXP = /:collapsed-lines\b/
+const COLLAPSED_LINES_START_REGEXP = /:collapsed-lines=(\d+)\b/
+const NO_COLLAPSED_LINES_REGEXP = /:no-collapsed-lines\b/
 
-const DEFAULT_LINES = 15
+/**
+ * Resolve the `:collapsed-lines` `:collapsed-lines=num` / `:no-collapsed-lines` mark from token info
+ */
+export function resolveCollapsedLines(info: string): boolean | number | null {
+  const lines = COLLAPSED_LINES_START_REGEXP.exec(info)?.[1]
 
-export function resolveCollapsedLines(info: string, defaultLines: boolean | number): number | false {
-  if (NO_COLLAPSED_LINES_REGEXP.test(info))
-    return false
-
-  const lines = defaultLines === true ? DEFAULT_LINES : defaultLines
-
-  const match = info.match(COLLAPSED_LINES_REGEXP)
-
-  if (match) {
-    return Number(match[1]) || lines || DEFAULT_LINES
+  if (lines) {
+    return Number(lines)
   }
-  return lines ?? false
+
+  if (COLLAPSED_LINES_REGEXP.test(info)) {
+    return true
+  }
+
+  if (NO_COLLAPSED_LINES_REGEXP.test(info)) {
+    return false
+  }
+
+  return null
 }

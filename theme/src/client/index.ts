a63ac17513598e8d182e16ebff28806c083a4ba2
pengzhanbo <volodymyr@foxmail.com>
2024-09-27 08:40:59 +0800

    perf(theme): improve theme exports, close #215 (#221)

--- theme/src/client/index.ts
+++ theme/src/client/index.ts
@@ -1,27 +1,25 @@
 export * from '../shared/index.js'
-export { default as VPBadge } from './components/global/VPBadge.vue'
 
+export { default as VPBadge } from './components/global/VPBadge.vue'
 export { default as VPCard } from './components/global/VPCard.vue'
+
 export { default as VPCardGrid } from './components/global/VPCardGrid.vue'
 export { default as VPHomeBanner } from './components/Home/VPHomeBanner.vue'
-
 export { default as VPHomeBox } from './components/Home/VPHomeBox.vue'
-
 export { default as VPHomeCustom } from './components/Home/VPHomeCustom.vue'
 export { default as VPHomeFeatures } from './components/Home/VPHomeFeatures.vue'
-
 export { default as VPHomeHero } from './components/Home/VPHomeHero.vue'
 export { default as VPHomeProfile } from './components/Home/VPHomeProfile.vue'
-export { default as VPHomeTextImage } from './components/Home/VPHomeTextImage.vue'
 
+export { default as VPHomeTextImage } from './components/Home/VPHomeTextImage.vue'
 export { default as VPButton } from './components/VPButton.vue'
 export { default as VPImage } from './components/VPImage.vue'
 export { default as VPLink } from './components/VPLink.vue'
 
 export * from './composables/index.js'
 export { default as plumeClientConfig } from './config.js'
+
 export { default as Layout } from './layouts/Layout.vue'
 export { default as NotFound } from './layouts/NotFound.vue'
+
 export * from './utils/index.js'
-export * from '@vuepress/plugin-comment/client'
-export * from 'vuepress-plugin-md-enhance/client'

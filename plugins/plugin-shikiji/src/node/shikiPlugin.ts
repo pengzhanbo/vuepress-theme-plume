6fac619031421daab0901c94145474b49dac9182
pengzhanbo <volodymyr@foxmail.com>
2024-10-03 09:27:14 +0800

    perf(plugin-md-power): add unset `languages` warning (#251)

--- plugins/plugin-shikiji/src/node/shikiPlugin.ts
+++ plugins/plugin-shikiji/src/node/shikiPlugin.ts
@@ -6,6 +6,7 @@ import type {
   PreWrapperOptions,
 } from './types.js'
 import { isPlainObject } from 'vuepress/shared'
+import { colors } from 'vuepress/utils'
 import { copyCodeButtonPlugin } from './copy-code-button/index.js'
 import { highlight, scanLanguages } from './highlight/index.js'
 import {
@@ -55,8 +56,10 @@ export function shikiPlugin({
 
       if (!options.languages || !options.languages.length) {
         options.languages = await scanLanguages(app)
+        if (options.languages.length) {
+          logger.warn(`You have not configured \`${colors.cyan('plugins.shiki.languages')}\`. It has been detected that you are using \`${colors.green(JSON.stringify(options.languages))}\`. Please add it to the \`${colors.cyan('plugins.shiki.languages')}\` configuration.`)
+        }
         if (app.env.isDebug) {
-          logger.info(`scan languages: ${JSON.stringify(options.languages)}`)
           logger.info(`scan languages in: ${(performance.now() - start).toFixed(2)}ms`)
         }
       }

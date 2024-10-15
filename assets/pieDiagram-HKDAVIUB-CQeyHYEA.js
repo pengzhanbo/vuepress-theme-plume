7b7476a3cff7f40a7c73d68271f1379434c6effd
pengzhanbo <q942450674@outlook.com>
2024-10-15 10:40:04 +0000

    Deploying to gh-pages from @ pengzhanbo/vuepress-theme-plume@0313a8e00eefe659758de81d1a0ffcabdfe1ae2b 🚀

---
+++ assets/pieDiagram-HKDAVIUB-CQeyHYEA.js
@@ -0,0 +1,30 @@
+import{c as V}from"./chunk-4KE642ED-RJoltWXd.js";import{p as X}from"./gitGraph-YCYPL57B-QPBUOVDP-C2emiKko.js";import{aq as q,m as r,v as G,J as y,aU as I,Q as K,ar as U,aS as Y,aV as j,aW as L,aX as H,k as Z,L as _,A as ee,w as te,B as ae,F as ie,M as re}from"./mermaid.esm.min-C69JTCNV.js";import"./chunk-BKDDFIKN-C2DW5Y7A.js";import"./app-D0yVB0Ip.js";var O=q.pie,D={sections:new Map,showData:!1,config:O},u=D.sections,v=D.showData,le=structuredClone(O),se=r(()=>structuredClone(le),"getConfig"),oe=r(()=>{u=new Map,v=D.showData,G()},"clear"),ne=r(({label:e,value:a})=>{u.has(e)||(u.set(e,a),y.debug(`added new section: ${e}, with value: ${a}`))},"addSection"),pe=r(()=>u,"getSections"),de=r(e=>{v=e},"setShowData"),ce=r(()=>v,"getShowData"),R={getConfig:se,clear:oe,setDiagramTitle:_,getDiagramTitle:ee,setAccTitle:te,getAccTitle:ae,setAccDescription:ie,getAccDescription:re,addSection:ne,getSections:pe,setShowData:de,getShowData:ce},fe=r((e,a)=>{V(e,a),a.setShowData(e.showData),e.sections.map(a.addSection)},"populateDb"),ge={parse:r(async e=>{let a=await X("pie",e);y.debug(a),fe(a,R)},"parse")},me=r(e=>`
+  .pieCircle{
+    stroke: ${e.pieStrokeColor};
+    stroke-width : ${e.pieStrokeWidth};
+    opacity : ${e.pieOpacity};
+  }
+  .pieOuterCircle{
+    stroke: ${e.pieOuterStrokeColor};
+    stroke-width: ${e.pieOuterStrokeWidth};
+    fill: none;
+  }
+  .pieTitleText {
+    text-anchor: middle;
+    font-size: ${e.pieTitleTextSize};
+    fill: ${e.pieTitleTextColor};
+    font-family: ${e.fontFamily};
+  }
+  .slice {
+    font-family: ${e.fontFamily};
+    fill: ${e.pieSectionTextColor};
+    font-size:${e.pieSectionTextSize};
+    // fill: white;
+  }
+  .legend text {
+    fill: ${e.pieLegendTextColor};
+    font-family: ${e.fontFamily};
+    font-size: ${e.pieLegendTextSize};
+  }
+`,"getStyles"),ue=me,he=r(e=>{let a=[...e.entries()].map(l=>({label:l[0],value:l[1]})).sort((l,h)=>h.value-l.value);return I().value(l=>l.value)(a)},"createPieArcs"),Se=r((e,a,l,h)=>{y.debug(`rendering pie chart
+`+e);let d=h.db,T=K(),C=U(d.getConfig(),T.pie),k=40,s=18,c=4,o=450,S=o,x=Y(a),n=x.append("g");n.attr("transform","translate("+S/2+","+o/2+")");let{themeVariables:i}=T,[b]=j(i.pieOuterStrokeWidth);b??(b=2);let A=C.textPosition,f=Math.min(S,o)/2-k,W=L().innerRadius(0).outerRadius(f),B=L().innerRadius(f*A).outerRadius(f*A);n.append("circle").attr("cx",0).attr("cy",0).attr("r",f+b/2).attr("class","pieOuterCircle");let M=d.getSections(),w=he(M),P=[i.pie1,i.pie2,i.pie3,i.pie4,i.pie5,i.pie6,i.pie7,i.pie8,i.pie9,i.pie10,i.pie11,i.pie12],p=H(P);n.selectAll("mySlices").data(w).enter().append("path").attr("d",W).attr("fill",t=>p(t.data.label)).attr("class","pieCircle");let z=0;M.forEach(t=>{z+=t}),n.selectAll("mySlices").data(w).enter().append("text").text(t=>(t.data.value/z*100).toFixed(0)+"%").attr("transform",t=>"translate("+B.centroid(t)+")").style("text-anchor","middle").attr("class","slice"),n.append("text").text(d.getDiagramTitle()).attr("x",0).attr("y",-(o-50)/2).attr("class","pieTitleText");let $=n.selectAll(".legend").data(p.domain()).enter().append("g").attr("class","legend").attr("transform",(t,g)=>{let m=s+c,E=m*p.domain().length/2,J=12*s,N=g*m-E;return"translate("+J+","+N+")"});$.append("rect").attr("width",s).attr("height",s).style("fill",p).style("stroke",p),$.data(w).append("text").attr("x",s+c).attr("y",s-c).text(t=>{let{label:g,value:m}=t.data;return d.getShowData()?`${g} [${m}]`:g});let Q=Math.max(...$.selectAll("text").nodes().map(t=>(t==null?void 0:t.getBoundingClientRect().width)??0)),F=S+k+s+c+Q;x.attr("viewBox",`0 0 ${F} ${o}`),Z(x,o,F,C.useMaxWidth)},"draw"),xe={draw:Se},Te={parser:ge,db:R,renderer:xe,styles:ue};export{Te as diagram};
---
+++ assets/stateDiagram-v2-OC7PCC74-DvOcMAie.js
@@ -0,0 +1,1 @@
+import{I as s,r as a,B as t,a as e}from"./chunk-HOLNC4VV-wWYABJNx.js";import"./chunk-BWM7Q77P-Cet93_V5.js";import{m as o}from"./mermaid.esm.min-C69JTCNV.js";import"./app-D0yVB0Ip.js";var l={parser:s,db:a,renderer:t,styles:e,init:o(r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute,a.clear()},"init")};export{l as diagram};

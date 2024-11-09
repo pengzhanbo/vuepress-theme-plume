55a103375f463d34b54c93a247c014914ca9f6e6
pengzhanbo <volodymyr@foxmail.com>
2024-11-10 00:11:04 +0800

    feat(theme): optimize appearance transition, close #325

--- theme/src/client/components/VPSwitchAppearance.vue
+++ theme/src/client/components/VPSwitchAppearance.vue
@@ -1,13 +1,79 @@
 <script lang="ts" setup>
 import VPSwitch from '@theme/VPSwitch.vue'
-import { inject, ref, watchPostEffect } from 'vue'
-import { useData } from '../composables/index.js'
+import { computed, inject, nextTick, ref, watchPostEffect } from 'vue'
+import { enableTransitions, useData } from '../composables/index.js'
 
 const checked = ref(false)
 const { theme, isDark } = useData()
 
-const toggleAppearance = inject('toggle-appearance', () => {
-  isDark.value = !isDark.value
+const transitionMode = computed(() => {
+  const transition = theme.value.transition
+  const options = typeof transition === 'object' ? transition : {}
+  if (transition === false || options.appearance === false)
+    return false
+
+  return typeof options.appearance === 'string' ? options.appearance : 'fade'
+})
+
+const toggleAppearance = inject('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
+  if (!enableTransitions() || transitionMode.value === false) {
+    isDark.value = !isDark.value
+    return
+  }
+
+  await document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  }).ready
+
+  const keyframes: PropertyIndexedKeyframes = {}
+  const mode = transitionMode.value
+  let duration = 400
+
+  if (mode === 'circle-clip') {
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${Math.hypot(
+        Math.max(x, innerWidth - x),
+        Math.max(y, innerHeight - y),
+      )}px at ${x}px ${y}px)`,
+    ]
+    keyframes.clipPath = isDark.value ? clipPath.reverse() : clipPath
+  }
+  else if (mode === 'horizontal-clip') {
+    const clipPath = [
+      `inset(0px ${innerWidth}px 0px 0px)`,
+      `inset(0px 0px 0px 0px)`,
+    ]
+    keyframes.clipPath = isDark.value ? clipPath.reverse() : clipPath
+  }
+  else if (mode === 'vertical-clip') {
+    const clipPath = [
+      `inset(0px 0px ${innerHeight}px 0px)`,
+      `inset(0px 0px 0px 0px)`,
+    ]
+    keyframes.clipPath = isDark.value ? clipPath.reverse() : clipPath
+  }
+  else if (mode === 'skew-clip') {
+    const clipPath = [
+      'polygon(0px 0px, 0px 0px, 0px 0px)',
+      `polygon(0px 0px, ${innerWidth * 2}px 0px, 0px ${innerHeight * 2}px)`,
+    ]
+    keyframes.clipPath = isDark.value ? clipPath.reverse() : clipPath
+  }
+  else {
+    keyframes.opacity = isDark.value ? [1, 0] : [0, 1]
+    duration = 300
+  }
+
+  document.documentElement.animate(
+    keyframes,
+    {
+      duration,
+      easing: 'ease-in',
+      pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`,
+    },
+  )
 })
 
 const switchTitle = ref('')
@@ -50,5 +116,23 @@ watchPostEffect(() => {
 [data-theme="dark"] .vp-switch-appearance :deep(.check) {
   /* rtl:ignore */
   transform: translateX(18px);
+}
+</style>
+
+<style>
+::view-transition-old(root),
+::view-transition-new(root) {
+  mix-blend-mode: normal;
+  animation: none;
+}
+
+::view-transition-old(root),
+[data-theme="dark"]::view-transition-new(root) {
+  z-index: 1;
+}
+
+::view-transition-new(root),
+[data-theme="dark"]::view-transition-old(root) {
+  z-index: 9999;
 }
 </style>

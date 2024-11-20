33a38c2f3db64ed668d722237e3edf4633082424
pengzhanbo <volodymyr@foxmail.com>
2024-11-20 20:53:14 +0800

    feat(theme): add multiple locales preset

---
+++ theme/src/node/locales/de.ts
@@ -0,0 +1,125 @@
+/** 德语 */
+import type { DocSearchLocaleOptions } from '@vuepress/plugin-docsearch'
+import type { SearchLocaleOptions } from '@vuepress-plume/plugin-search'
+import type { PlumeThemeLocaleData, PresetLocale } from '../../shared/index.js'
+
+export const deLocale: PlumeThemeLocaleData = {
+  selectLanguageName: 'Deutsch',
+  selectLanguageText: 'Sprache auswählen',
+
+  appearanceText: 'Erscheinungsbild',
+  lightModeSwitchTitle: 'Zu hellem Thema wechseln',
+  darkModeSwitchTitle: 'Zu dunklem Thema wechseln',
+
+  outlineLabel: 'Inhalt dieser Seite',
+  returnToTopLabel: 'Zurück nach oben',
+  editLinkText: 'Diese Seite bearbeiten',
+  contributorsText: 'Mitwirkende',
+  prevPageLabel: 'Vorherige Seite',
+  nextPageLabel: 'Nächste Seite',
+  lastUpdatedText: 'Zuletzt aktualisiert am',
+
+  changelogText: 'Änderungsprotokoll',
+  changelogOnText: 'am',
+  changelogButtonText: 'Alle Änderungen anzeigen',
+
+  copyrightText: 'Alle Rechte vorbehalten',
+  copyrightAuthorText: 'Urheberrecht liegt bei:',
+  copyrightCreationOriginalText: 'Originalartikel:',
+  copyrightCreationTranslateText: 'Übersetzt aus:',
+  copyrightCreationReprintText: 'Nachdruck von:',
+  copyrightLicenseText: 'Lizenz:',
+
+  notFound: {
+    code: '404',
+    title: 'Seite nicht gefunden',
+    quote: 'Aber wenn du deine Richtung nicht änderst und weiter suchst, könntest du schließlich dorthin gelangen, wohin du gehen willst.',
+    linkText: 'Zur Startseite',
+  },
+
+  encryptButtonText: 'Bestätigen',
+  encryptPlaceholder: 'Bitte Passwort eingeben',
+  encryptGlobalText: 'Diese Website ist nur mit Passwort zugänglich',
+  encryptPageText: 'Diese Seite ist nur mit Passwort zugänglich',
+
+  footer: {
+    message:
+      'Unterstützt von <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a>',
+  },
+}
+
+export const dePresetLocale: PresetLocale = {
+  'home': 'Startseite',
+  'blog': 'Blog',
+  'tag': 'Tag',
+  'archive': 'Archiv',
+  'category': 'Kategorie',
+  'archiveTotal': '{count} Beiträge',
+
+  // ------ copyright license ------
+  'CC0': 'CC0 1.0 Universell',
+  'CC-BY-4.0': 'Namensnennung 4.0 International',
+  'CC-BY-NC-4.0': 'Namensnennung-Nicht kommerziell 4.0 International',
+  'CC-BY-NC-SA-4.0': 'Namensnennung-Nicht kommerziell-Weitergabe unter gleichen Bedingungen 4.0 International',
+  'CC-BY-NC-ND-4.0': 'Namensnennung-Nicht kommerziell-Keine Bearbeitung 4.0 International',
+  'CC-BY-ND-4.0': 'Namensnennung-Keine Bearbeitung 4.0 International',
+  'CC-BY-SA-4.0': 'Namensnennung-Weitergabe unter gleichen Bedingungen 4.0 International',
+}
+
+export const deDocsearchLocale: DocSearchLocaleOptions = {
+  placeholder: 'Dokumente durchsuchen',
+  translations: {
+    button: {
+      buttonText: 'Dokumente durchsuchen',
+      buttonAriaLabel: 'Dokumente durchsuchen',
+    },
+    modal: {
+      searchBox: {
+        resetButtonTitle: 'Suchkriterien löschen',
+        resetButtonAriaLabel: 'Suchkriterien löschen',
+        cancelButtonText: 'Abbrechen',
+        cancelButtonAriaLabel: 'Abbrechen',
+      },
+      startScreen: {
+        recentSearchesTitle: 'Letzte Suchen',
+        noRecentSearchesText: 'Keine letzten Suchen',
+        saveRecentSearchButtonTitle: 'Zu letzten Suchen hinzufügen',
+        removeRecentSearchButtonTitle: 'Aus letzten Suchen entfernen',
+        favoriteSearchesTitle: 'Favoriten',
+        removeFavoriteSearchButtonTitle: 'Aus Favoriten entfernen',
+      },
+      errorScreen: {
+        titleText: 'Ergebnisse konnten nicht abgerufen werden',
+        helpText: 'Möglicherweise müssen Sie Ihre Internetverbindung überprüfen.',
+      },
+      footer: {
+        selectText: 'Auswählen',
+        navigateText: 'Wechseln',
+        closeText: 'Schließen',
+        searchByText: 'Bereitgestellt von',
+      },
+      noResultsScreen: {
+        noResultsText: 'Keine relevanten Ergebnisse gefunden',
+        suggestedQueryText: 'Sie können versuchen, nach',
+        reportMissingResultsText: 'Sie glauben, dass diese Suche Ergebnisse liefern sollte?',
+        reportMissingResultsLinkText: 'Klicken Sie hier, um Feedback zu geben',
+      },
+    },
+  },
+}
+
+export const deSearchLocale: Partial<SearchLocaleOptions> = {
+  placeholder: 'Dokumente durchsuchen',
+  resetButtonTitle: 'Suche zurücksetzen',
+  backButtonTitle: 'Schließen',
+  noResultsText: 'Keine Suchergebnisse:',
+  footer: {
+    selectText: 'Auswählen',
+    selectKeyAriaLabel: 'Eingabe',
+    navigateText: 'Wechseln',
+    navigateUpKeyAriaLabel: 'Nach oben',
+    navigateDownKeyAriaLabel: 'Nach unten',
+    closeText: 'Schließen',
+    closeKeyAriaLabel: 'Beenden',
+  },
+}
---
+++ theme/src/node/locales/ja.ts
@@ -0,0 +1,125 @@
+/** 日语 */
+import type { DocSearchLocaleOptions } from '@vuepress/plugin-docsearch'
+import type { SearchLocaleOptions } from '@vuepress-plume/plugin-search'
+import type { PlumeThemeLocaleData, PresetLocale } from '../../shared/index.js'
+
+export const jaLocale: PlumeThemeLocaleData = {
+  selectLanguageName: '日本語',
+  selectLanguageText: '言語を選択',
+
+  appearanceText: '外観',
+  lightModeSwitchTitle: 'ライトモードに切り替え',
+  darkModeSwitchTitle: 'ダークモードに切り替え',
+
+  outlineLabel: 'このページの内容',
+  returnToTopLabel: 'トップに戻る',
+  editLinkText: 'このページを編集',
+  contributorsText: '貢献者',
+  prevPageLabel: '前のページ',
+  nextPageLabel: '次のページ',
+  lastUpdatedText: '最終更新日',
+
+  changelogText: '変更履歴',
+  changelogOnText: 'に',
+  changelogButtonText: 'すべての変更履歴を見る',
+
+  copyrightText: '著作権',
+  copyrightAuthorText: '著作権者：',
+  copyrightCreationOriginalText: '本文リンク：',
+  copyrightCreationTranslateText: '本文の翻訳元：',
+  copyrightCreationReprintText: '本文の転載元：',
+  copyrightLicenseText: 'ライセンス：',
+
+  notFound: {
+    code: '404',
+    title: 'ページが見つかりません',
+    quote: 'しかし、方向を変えずに探し続ければ、最終的には行きたい場所にたどり着くかもしれません。',
+    linkText: 'ホームに戻る',
+  },
+
+  encryptButtonText: '確認',
+  encryptPlaceholder: 'パスワードを入力してください',
+  encryptGlobalText: 'このサイトはパスワードでのみアクセス可能です',
+  encryptPageText: 'このページはパスワードでのみアクセス可能です',
+
+  footer: {
+    message:
+      '<a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a> によって提供されています',
+  },
+}
+
+export const jaPresetLocale: PresetLocale = {
+  'home': 'ホーム',
+  'blog': 'ブログ',
+  'tag': 'タグ',
+  'archive': 'アーカイブ',
+  'category': 'カテゴリー',
+  'archiveTotal': '{count} 件',
+
+  // ------ copyright license ------
+  'CC0': 'CC0 1.0 パブリックドメイン',
+  'CC-BY-4.0': '表示 4.0 国際',
+  'CC-BY-NC-4.0': '表示-非営利 4.0 国際',
+  'CC-BY-NC-SA-4.0': '表示-非営利-継承 4.0 国際',
+  'CC-BY-NC-ND-4.0': '表示-非営利-改変禁止 4.0 国際',
+  'CC-BY-ND-4.0': '表示-改変禁止 4.0 国際',
+  'CC-BY-SA-4.0': '表示-継承 4.0 国際',
+}
+
+export const jaDocsearchLocale: DocSearchLocaleOptions = {
+  placeholder: 'ドキュメントを検索',
+  translations: {
+    button: {
+      buttonText: 'ドキュメントを検索',
+      buttonAriaLabel: 'ドキュメントを検索',
+    },
+    modal: {
+      searchBox: {
+        resetButtonTitle: 'クエリをクリア',
+        resetButtonAriaLabel: 'クエリをクリア',
+        cancelButtonText: 'キャンセル',
+        cancelButtonAriaLabel: 'キャンセル',
+      },
+      startScreen: {
+        recentSearchesTitle: '最近の検索',
+        noRecentSearchesText: '最近の検索はありません',
+        saveRecentSearchButtonTitle: '最近の検索に保存',
+        removeRecentSearchButtonTitle: '最近の検索から削除',
+        favoriteSearchesTitle: 'お気に入り',
+        removeFavoriteSearchButtonTitle: 'お気に入りから削除',
+      },
+      errorScreen: {
+        titleText: '結果を取得できません',
+        helpText: 'ネットワーク接続を確認する必要があるかもしれません',
+      },
+      footer: {
+        selectText: '選択',
+        navigateText: '切り替え',
+        closeText: '閉じる',
+        searchByText: '検索提供者',
+      },
+      noResultsScreen: {
+        noResultsText: '関連する結果が見つかりません',
+        suggestedQueryText: 'クエリを試すことができます',
+        reportMissingResultsText: 'このクエリに結果があると思いますか？',
+        reportMissingResultsLinkText: 'フィードバックを送信',
+      },
+    },
+  },
+}
+
+export const jaSearchLocale: Partial<SearchLocaleOptions> = {
+  placeholder: 'ドキュメントを検索',
+  resetButtonTitle: '検索をリセット',
+  backButtonTitle: '閉じる',
+  noResultsText: '検索結果がありません：',
+  footer: {
+    selectText: '選択',
+    selectKeyAriaLabel: '入力',
+    navigateText: '切り替え',
+    navigateUpKeyAriaLabel: '上へ',
+    navigateDownKeyAriaLabel: '下へ',
+    closeText: '閉じる',
+    closeKeyAriaLabel: '終了',
+  },
+}

6a8ba55bbf960107f726f794841f87142edd6395
pengzhanbo <volodymyr@foxmail.com>
2025-02-01 00:51:30 +0800

    chore: update stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,16 +2,9 @@ import { stylelintConfig } from '@pengzhanbo/stylelint-config'
 
 export default stylelintConfig({
   rules: {
-    'selector-pseudo-element-no-unknown': [true, {
-      ignorePseudoElements: ['input-placeholder'],
-    }],
-    // 'no-descending-specificity': null,
     'custom-property-empty-line-before': null,
     'property-no-vendor-prefix': null,
     'selector-no-vendor-prefix': null,
-    'property-no-unknown': [true, {
-      ignoreProperties: ['r', 'font-named-instance'],
-    }],
     'declaration-block-no-redundant-longhand-properties': [true, {
       ignoreShorthands: ['inset'],
     }],

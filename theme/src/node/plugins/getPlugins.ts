b196e24ff2c626ff3eb58a7b6eec760dc7bf576d
Mister-Hope <mister-hope@outlook.com>
2025-02-28 18:28:25 +0800

    feat: remove shiki plugin

--- theme/src/node/plugins/getPlugins.ts
+++ theme/src/node/plugins/getPlugins.ts
@@ -4,10 +4,10 @@ import type { App, PluginConfig } from 'vuepress/core'
 import type { PlumeThemePluginOptions } from '../../shared/index.js'
 import { fontsPlugin } from '@vuepress-plume/plugin-fonts'
 import { searchPlugin } from '@vuepress-plume/plugin-search'
-import { shikiPlugin } from '@vuepress-plume/plugin-shikiji'
 import { isPlainObject } from '@vuepress/helper'
 import { cachePlugin } from '@vuepress/plugin-cache'
 import { commentPlugin } from '@vuepress/plugin-comment'
+import { copyCodePlugin } from '@vuepress/plugin-copy-code'
 import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 import { gitPlugin } from '@vuepress/plugin-git'
 import { markdownHintPlugin } from '@vuepress/plugin-markdown-hint'
@@ -18,6 +18,7 @@ import { nprogressPlugin } from '@vuepress/plugin-nprogress'
 import { photoSwipePlugin } from '@vuepress/plugin-photo-swipe'
 import { readingTimePlugin } from '@vuepress/plugin-reading-time'
 import { seoPlugin } from '@vuepress/plugin-seo'
+import { shikiPlugin } from '@vuepress/plugin-shiki'
 import { sitemapPlugin } from '@vuepress/plugin-sitemap'
 import { watermarkPlugin } from '@vuepress/plugin-watermark'
 import { mdEnhancePlugin } from 'vuepress-plugin-md-enhance'
@@ -102,12 +103,17 @@ export function getPlugins({
     plugins.push(searchPlugin(pluginOptions.search || {}))
   }
 
+  if (pluginOptions.copyCode !== false) {
+    plugins.push(copyCodePlugin(pluginOptions.copyCode || {}))
+  }
+
   const shikiOption = pluginOptions.shiki
-  let shikiTheme: any = { light: 'vitesse-light', dark: 'vitesse-dark' }
+
+  const shikiTheme = shikiOption && 'theme' in shikiOption ? shikiOption.theme : shikiOption && 'themes' in shikiOption ? shikiOption.themes : { light: 'vitesse-light', dark: 'vitesse-dark' }
+
   if (shikiOption !== false) {
-    shikiTheme = shikiOption?.theme ?? shikiTheme
     plugins.push(shikiPlugin({
-      theme: shikiTheme,
+      ...(shikiOption && 'theme' in shikiOption ? {} : { themes: { light: 'vitesse-light', dark: 'vitesse-dark' } }),
       ...(shikiOption ?? {}),
     }))
   }

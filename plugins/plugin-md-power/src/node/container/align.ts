2370986bbf34787a39ef0eed0d0ed12c79866bd5
pengzhanbo <volodymyr@foxmail.com>
2025-05-16 21:08:39 +0800

    feat(plugin-md-power): add flex container

--- plugins/plugin-md-power/src/node/container/align.ts
+++ plugins/plugin-md-power/src/node/container/align.ts
@@ -1,4 +1,6 @@
 import type { Markdown } from 'vuepress/markdown'
+import { parseRect } from '../utils/parseRect.js'
+import { resolveAttrs } from '../utils/resolveAttrs.js'
 import { createContainerPlugin } from './createContainer.js'
 
 const alignList = ['left', 'center', 'right', 'justify']
@@ -9,4 +11,41 @@ export function alignPlugin(md: Markdown): void {
       before: () => `<div style="text-align:${name}">`,
     })
   }
+
+  createContainerPlugin(md, 'flex', {
+    before: (info) => {
+      const { attrs } = resolveAttrs<FlexContainerAttrs>(info)
+      const styles: string[] = ['margin:16px 0;display:flex']
+
+      const align = attrs.start ? 'flex-start' : attrs.end ? 'flex-end' : attrs.center ? 'center' : ''
+      const justify = attrs.between ? 'space-between' : attrs.around ? 'space-around' : attrs.center ? 'center' : ''
+
+      if (align)
+        styles.push(`align-items:${align}`)
+
+      if (justify)
+        styles.push(`justify-content:${justify}`)
+
+      if (attrs.column)
+        styles.push('flex-direction:column')
+
+      if (attrs.wrap)
+        styles.push('flex-wrap:wrap')
+
+      styles.push(`gap:${parseRect(attrs.gap || '16')}`)
+
+      return `<div style="${styles.join(';')}">`
+    },
+  })
+}
+
+interface FlexContainerAttrs {
+  center?: boolean
+  wrap?: boolean
+  between?: boolean
+  around?: boolean
+  start?: boolean
+  end?: boolean
+  gap?: string
+  column?: boolean
 }

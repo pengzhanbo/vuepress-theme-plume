5eda4e6e5a6c8b52d849ade3001233f8f8ccec7d
huyunan <18842920976@163.com>
2025-09-13 13:25:20 +0800

    feat(theme): signdown parameter adds whether to display a downward arrow flag

--- theme/src/client/components/VPSignDown.vue
+++ theme/src/client/components/VPSignDown.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
+import type { ThemeHomeConfig } from 'theme/src/shared/index.js'
 import { useElementSize, useWindowScroll, useWindowSize } from '@vueuse/core'
 import { computed, onMounted, shallowRef } from 'vue'
 import { useData } from '../composables/index.js'
-import '@vuepress/helper/transition/fade-in.css'
 
 const body = shallowRef<HTMLElement | null>()
 const { height: bodyHeight } = useElementSize(body)
@@ -11,57 +11,73 @@ onMounted(() => {
   body.value = document.body
 })
 
-const { page } = useData()
+const { page, isDark } = useData()
 const { y } = useWindowScroll()
 
-const progress = computed(
-  () => (y.value / (bodyHeight.value - windowHeight.value)) * 100,
-)
-
-const percent = computed(() => Math.min(Math.round(progress.value), 90) || 0)
-
 const mustHidden = computed(() => {
-  return page.value.frontmatter.signDown === undefined || page.value.frontmatter.signDown === false || (page.value.frontmatter.pageLayout === 'home' && page.value.frontmatter.config && (page.value.frontmatter.config as any).length <= 1)
+  if (page.value.frontmatter.signDown !== true || page.value.frontmatter.pageLayout !== 'home' || !page.value.frontmatter.config) {
+    return true
+  }
+  const homeConfig = page.value.frontmatter.config as ThemeHomeConfig[]
+  if (homeConfig.length <= 1 || homeConfig[0].full !== true) {
+    return true
+  }
+  return false
 })
 
 const show = computed(() => {
   if (bodyHeight.value < windowHeight.value)
     return false
-  else if (percent.value === 90)
-    return false
-  else
+  else if (y.value < 160)
     return true
+  else
+    return false
 })
 </script>
 
 <template>
-  <svg v-show="!mustHidden && show" aria-label="sign down" t="1755683953015" class="vp-sign-down" aria-hidden="true" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3080" width="48" height="48">
-    <path d="M597.333 640V85.333c0-23.68-19.285-42.666-43.093-42.666h-84.48a42.667 42.667 0 0 0-43.093 42.666V640h-84.95c-47.232 0-62.805 30.55-34.56 68.267l153.643 204.8c28.501 37.973 74.112 37.717 102.4 0l153.6-204.8C745.301 670.293 729.6 640 682.283 640h-84.95z" fill="#333333" p-id="3081" />
+  <svg v-show="!mustHidden && show" aria-label="sign down" class="vp-sign-down" :class="{ 'vp-sign-down-dark': isDark }" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <g fill="currentColor" fill-rule="evenodd" clip-rule="evenodd">
+      <path d="M16.884 5.348a1.25 1.25 0 0 1 0 1.768L12 12L7.116 7.116a1.25 1.25 0 0 1 1.768-1.768L12 8.464l3.116-3.116a1.25 1.25 0 0 1 1.768 0" />
+      <path d="M16.884 12.366a1.25 1.25 0 0 1 0 1.768L12 19.018l-4.884-4.884a1.25 1.25 0 0 1 1.768-1.768L12 15.482l3.116-3.116a1.25 1.25 0 0 1 1.768 0" />
+    </g>
   </svg>
 </template>
 
 <style scoped>
-@keyframes blink {
-  0%, 100% { opacity: 1; }
-  50% { opacity: 0; }
+@keyframes top-bot {
+  0%,
+  100% {
+    transform: translate(0, -15px);
+  }
+
+  50% {
+    transform: translate(0, 0);
+  }
 }
 
 .vp-sign-down {
-  width: 22px;
-  height: 22px;
   position: fixed;
-  bottom: 20px;
+  bottom: 10px;
   left: 50%;
-  transform: translate(-50%, 0);
   z-index: 100;
-  animation: blink 1.2s infinite;
+  width: 26px;
+  height: 26px;
+  color: white;
+  opacity: 0.8;
+  transform: translate(-50%, 0);
+  animation: top-bot 3s infinite;
+}
+
+.vp-sign-down-dark {
+  color: gray;
 }
 
 @media (min-width: 768px) {
   .vp-sign-down {
     bottom: 15px;
-    width: 28px;
-    height: 28px;
+    width: 34px;
+    height: 34px;
   }
 }
 

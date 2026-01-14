3073e2d43acc985a391ef3a4661550d2eccbc45c
pengzhanbo <volodymyr@foxmail.com>
2026-01-14 16:36:50 +0800

    feat(theme): add perplexity to context menu

--- theme/src/client/features/components/PageContextMenu.vue
+++ theme/src/client/features/components/PageContextMenu.vue
@@ -14,9 +14,22 @@ interface MenuItem {
   type?: string
 }
 
-const { claude = true, chatgpt = true } = defineProps<{
+const { claude = true, chatgpt = true, perplexity = true } = defineProps<{
+  /**
+   * Claude AI
+   * @see https://claude.ai/new
+   */
   claude?: boolean
+  /**
+   * Chat GPT
+   * @see https://chat.openai.com
+   */
   chatgpt?: boolean
+  /**
+   * Perplexity
+   * @see https://perplexity.ai/
+   */
+  perplexity?: boolean
 }>()
 
 const { page, frontmatter, theme } = useData()
@@ -67,6 +80,15 @@ const menuList = computed(() => {
       text: (theme.value.askAIText ?? 'Open in {name}').replace('{name}', 'Claude'),
       tagline: (theme.value.askAITagline ?? 'Ask {name} about this page').replace('{name}', 'Claude'),
       icon: 'vpi-claude',
+    })
+  }
+
+  if (perplexity) {
+    list.push({
+      link: `https://perplexity.ai/?q=${message.value}`,
+      text: (theme.value.askAIText ?? 'Open in {name}').replace('{name}', 'Perplexity'),
+      tagline: (theme.value.askAITagline ?? 'Ask {name} about this page').replace('{name}', 'Perplexity'),
+      icon: 'vpi-perplexity',
     })
   }
 
@@ -307,5 +329,9 @@ const copyPageText = computed(() => {
 
 .vpi-claude {
   --icon: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath fill='%23000' d='m5.92 15.3l3.94-2.2l.06-.2l-.06-.1h-.2L9 12.76l-2.24-.06l-1.96-.1l-1.9-.1l-.48-.1l-.42-.6l.04-.3l.4-.26l.58.04l1.26.1l1.9.12l1.38.08l2.04.24h.32l.04-.14l-.1-.08l-.08-.08L7.8 10.2L5.68 8.8l-1.12-.82l-.6-.4l-.3-.4l-.12-.84l.54-.6l.74.06l.18.04l.74.58l1.6 1.22L9.4 9.2l.3.24l.12-.08l.02-.06l-.14-.22L8.6 7L7.4 4.92l-.54-.86l-.14-.52c-.06-.2-.08-.4-.08-.6l.6-.84l.36-.1l.84.12l.32.28l.52 1.2l.82 1.86l1.3 2.52l.4.76l.2.68l.06.2h.14v-.1l.1-1.44l.2-1.74l.2-2.24l.06-.64l.32-.76l.6-.4l.52.22l.4.58l-.06.36L14.32 5l-.52 2.42l-.3 1.64h.18l.2-.22l.82-1.08l1.38-1.72l.6-.7l.72-.74l.46-.36h.86l.62.94l-.28.98l-.88 1.12l-.74.94l-1.06 1.42l-.64 1.14l.06.08h.14l2.4-.52l1.28-.22l1.52-.26l.7.32l.08.32l-.28.68l-1.64.4l-1.92.4l-2.86.66l-.04.02l.04.06l1.28.12l.56.04h1.36l2.52.2l.66.4l.38.54l-.06.4l-1.02.52l-1.36-.32l-3.2-.76l-1.08-.26h-.16v.08l.92.9l1.66 1.5l2.12 1.94l.1.48l-.26.4l-.28-.04l-1.84-1.4l-.72-.6l-1.6-1.36h-.1v.14l.36.54l1.96 2.94l.1.9l-.14.28l-.52.2l-.54-.12l-1.16-1.6l-1.2-1.8l-.94-1.64l-.1.08l-.58 6.04l-.26.3l-.6.24l-.5-.4l-.28-.6l.28-1.24l.32-1.6l.26-1.28l.24-1.58l.14-.52v-.04h-.14l-1.2 1.66l-1.8 2.46l-1.44 1.52l-.34.14l-.6-.3l.06-.56l.32-.46l2-2.56l1.2-1.58l.8-.92l-.02-.1h-.06l-5.28 3.44l-.94.12l-.4-.4l.04-.6l.2-.2l1.6-1.1z'/%3E%3C/svg%3E");
+}
+
+.vpi-perplexity {
+  --icon: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath fill='%23000' d='M7.173 3.26L5.5 1.74V7.5H3V17h2.5v5.26l1.673-1.52L11 17.26V22h2v-4.74l3.827 3.48l1.673 1.52V17H21V7.5h-2.5V1.74l-1.673 1.52L13 6.74V2h-2v4.74zM16.5 7.5h-1.363L16.5 6.26zm-7.637 0H7.5V6.26zm1.1 2l-4.136 3.76l-.327.298V15H5V9.5zm4.074 0H19V15h-.5v-1.442l-.327-.298zM7.5 14.442l3.5-3.181v3.297L7.5 17.74zm5.5-3.181l3.5 3.181v3.297L13 14.558z'/%3E%3C/svg%3E");
 }
 </style>

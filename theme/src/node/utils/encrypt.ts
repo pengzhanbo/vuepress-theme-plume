886eec8494dda0c0b2a430cd4037fa490f4a3d2e
pengzhanbo <volodymyr@foxmail.com>
2026-01-14 16:58:02 +0800

    fix(theme): fix page encrypt password type error

--- theme/src/node/utils/encrypt.ts
+++ theme/src/node/utils/encrypt.ts
@@ -5,7 +5,7 @@ export async function genEncrypt(password: string): Promise<string> {
   const salt = new Uint8Array(16)
   crypto.getRandomValues(salt)
   return await bcrypt({
-    password,
+    password: String(password),
     salt,
     costFactor: 11,
     outputType: 'encoded',

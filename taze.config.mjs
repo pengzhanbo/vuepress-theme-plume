7d632038d524f02eb682f050c50ab34b039aebfd
pengzhanbo <volodymyr@foxmail.com>
2026-08-12 21:54:45 +0800

    chore: add taze config

---
+++ taze.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'taze'
+
+export default defineConfig({
+  exclude: [
+    'typescript',
+    'markdown-it',
+    'conventional-changelog',
+    'conventional-changelog-angular',
+  ],
+})

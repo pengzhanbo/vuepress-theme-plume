7903170956c7241c5054253d7030f9abace7f22e
pengzhanbo <volodymyr@foxmail.com>
2026-09-17 14:08:58 +0800

    fix(plugin-md-power): fix demo embed filepath

--- plugins/plugin-md-power/src/node/demo/extendPage.ts
+++ plugins/plugin-md-power/src/node/demo/extendPage.ts
@@ -1,17 +1,26 @@
 import type { Page } from 'vuepress'
 import type { MarkdownDemoEnv } from '../../shared/demo.js'
+import { ensureEndingSlash } from 'vuepress/shared'
+import { path } from 'vuepress/utils'
 
 export function extendsPageWithDemo(page: Page): void {
   const markdownEnv = page.markdownEnv as MarkdownDemoEnv
   const demoFiles = markdownEnv.demoFiles ?? []
 
-  page.deps.push(
-    ...demoFiles
-      .filter(({ type }) => type === 'markdown')
-      .map(({ path }) => path),
-  )
+  const deps = demoFiles
+    .filter(({ type }) => type === 'markdown')
+    .map(({ path }) => path)
 
-  ;((page.frontmatter.gitInclude as string[] | undefined) ??= []).push(
-    ...demoFiles.filter(({ gitignore }) => !gitignore).map(({ path }) => path),
-  )
+  if (deps.length) {
+    page.deps.push(...deps)
+  }
+
+  const gitIncludes = demoFiles
+    .filter(({ gitignore }) => !gitignore)
+    .map(demo => page.filePath && demo.path.replace(ensureEndingSlash(path.dirname(page.filePath)), ''))
+    .filter(item => item && item[0] !== '/') as string[]
+
+  if (gitIncludes.length) {
+    ((page.frontmatter.gitInclude as string[] | undefined) ??= []).push(...gitIncludes)
+  }
 }
